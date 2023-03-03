a264cbe9db06ff7c63835fd0e9e2b3ac5dfe3da9
svt13 <svt13@ukr.net>
2023-03-03 21:05:26 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Index from './layouts/Index';
+import Authorization from './layouts/Authorization';
+import WaterResistance from './layouts/WaterResistance';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Manager from './layouts/Manager';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid p-0 m-0">
+      <Router>
+        <Routes>
+          <Route path="/auth" element={ <Authorization /> } />
+          {/* <Route path="/" exact element={ <Index /> } />         */}
+          <Route path="/manager" element={ <Manager /> }>        
+            <Route path="waterresistance" element={ <WaterResistance /> } />  
+          </Route>      
+        </Routes>
+      </Router>
     </div>
   );
 }
