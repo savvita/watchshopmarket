952cb4cbe88edc8f64a35539fbc930d7f0f7635f
svt13 <svt13@ukr.net>
2023-04-13 17:30:56 +0300

    add adaptive for sidebar

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -6,16 +6,30 @@ import { selectCurrent as selectUser } from '../app/authSlice';
 import { useSelector } from "react-redux";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Admin = () => {
     const user = useSelector(selectUser);
+    const [collapsed, setCollapsed] = useState(false);
+
+
     return (
         <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
             <Header />
-            <div className="flex-grow-1">
+            <div className="flex-grow-1 d-flex">
                 { user && user.isAdmin && user.isActive && !user.expired ? 
-                    <div className="d-flex">
-                        <div className="p-3"><AdminSidebar /></div>
+                    <div className="d-flex position-relative flex-grow-1">
+                        <div className="d-flex flex-column">
+                            <div className="overflow-hidden p-2 m-3 mb-0 sidebar__btn">
+                                <FaBars className="property-table__icon" onClick={ () => setCollapsed(!collapsed) } />
+                            </div>
+                            <div className="position-relative flex-grow-1">
+                                <div className={ collapsed ? "p-3 border-end sidebar__collapsed bg-dark h-100" : "p-3 border-end sidebar__collapsed sidebar__opened bg-dark h-100" }>
+                                    <AdminSidebar />
+                                </div>
+                            </div>
+                        </div>
                         <div className="flex-grow-1 ps-4 pe-4" style={{ maxWidth: '1320px' }}><Outlet /></div>
                     </div>
                     :
--- src/layouts/Manager.js
+++ src/layouts/Manager.js
@@ -6,23 +6,36 @@ import Header from "../components/Header/Header";
 import { selectCurrent as selectUser } from '../app/authSlice';
 import { useSelector } from "react-redux";
 import Footer from "../components/Footer/Footer";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 const Manager = () => {
     const user = useSelector(selectUser);
+    const [collapsed, setCollapsed] = useState(false);
+
     return (
         <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-            <Header />
-            <div className="flex-grow-1">
+        <Header />
+        <div className="flex-grow-1 d-flex">
             { user && user.isManager && user.isActive && !user.expired ? 
-                <div className="d-flex">
-                    <div className="p-3"><ManagerSidebar /></div>
+                <div className="d-flex position-relative flex-grow-1">
+                    <div className="d-flex flex-column">
+                        <div className="overflow-hidden p-2 m-3 mb-0 sidebar__btn">
+                            <FaBars className="property-table__icon" onClick={ () => setCollapsed(!collapsed) } />
+                        </div>
+                        <div className="position-relative flex-grow-1">
+                            <div className={ collapsed ? "p-3 border-end sidebar__collapsed bg-dark" : "p-3 border-end sidebar__collapsed sidebar__opened bg-dark" } style={{ minHeight: '100%' }}>
+                                <ManagerSidebar />
+                            </div>
+                        </div>
+                    </div>
                     <div className="flex-grow-1 ps-4 pe-4" style={{ maxWidth: '1320px' }}><Outlet /></div>
                 </div>
                 :
                 <p className="text-white text-center mt-3">У вас немає прав для перегляду даної сторінки</p>
             }
-            </div>
-            <Footer />
+        </div>
+        <Footer />
         </div>
     );
 }
